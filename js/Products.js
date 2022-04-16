30992da596f36f463704513a01ec82b105bdb60b
sysionng <sysionng@gmail.com>
2022-04-16 15:50:41 +0100

    Completed js code to add product items and added some product image assets

--- js/Products.js
+++ js/Products.js
@@ -1,77 +1,77 @@
-let products = {
+let Products = {
   1: {
-    name: "MokBook Thicc",
-    img: "../image/1.png",
+    name: "Product-01",
+    img: "./image/1.png",           //this path is in relation to index.html
     desc: "first product item",
     price : 123.45
   },
   2: {
-    name: "MokBook Thicc",
-    img: "../image/2.png",
+    name: "Product-02",
+    img: "./image/2.png",
     desc: "second product item",
     price : 134.56
   },
   3: {
-    name: "MokBook Thicc",
-    img: "../image/3.png",
+    name: "Product-03",
+    img: "./image/3.png",
     desc: "third product item",
     price : 145.67
   },
   4: {
-    name: "MokBook Thicc",
-    img: "../image/4.png",
+    name: "Product-04",
+    img: "./image/4.png",
     desc: "fourth product item",
     price : 156.78
   },
   5: {
-    name: "MokBook Thicc",
-    img: "../image/5.png",
+    name: "Product-05",
+    img: "./image/5.png",
     desc: "fifth product item",
     price : 167.89
   },
   6: {
-    name: "MokBook Thicc",
-    img: "../image/6.png",
+    name: "Product-06",
+    img: "./image/6.png",
     desc: "sixth product item",
     price : 178.90
   },
   7: {
-    name: "MokBook Thicc",
-    img: "../image/7.png",
+    name: "Product-07",
+    img: "./image/7.png",
     desc: "seventh product item",
     price : 189.01
   },
   8: {
-    name: "MokBook Thicc",
-    img: "../image/8.png",
+    name: "Product-08",
+    img: "./image/8.png",
     desc: "eight product item",
     price : 190.12
   },
   9: {
-    name: "MokBook Thicc",
-    img: "../image/9.png",
+    name: "Product-09",
+    img: "./image/9.png",
     desc: "nineth product item",
     price : 101.23
   },
   10: {
-    name: "MokBook Thicc",
-    img: "../image/10.png",
+    name: "Product-10",
+    img: "./image/10.png",
     desc: "tenth product item",
     price : 112.34
   },
   11: {
-    name: "MokBook Thicc",
-    img: "../image/11.png",
+    name: "Product-11",
+    img: "./image/11.png",
     desc: "eleventh product item",
     price : 113.34
   },
   12: {
-    name: "MokBook Thicc",
-    img: "../image/12.png",
+    name: "Product-12",
+    img: "./image/12.png",
     desc: "twelveth product item",
     price : 124.36
   }
 
 };
 
-export default products;
+export default Products;
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-import products from "./products.js";
+import Products from "./Products.js";
 
 
 const main = document.querySelector('main');
 const btn_label = "Add To Cart";
+const img_alt = "Image for ";
 
 function createSection(product){
 	var section = document.createElement('section');
@@ -12,8 +13,18 @@ function createSection(product){
 	var section_price = document.createElement('p');
 	var section_button = document.createElement('button');
 
+	section.classList.add('product-item');
+	section_title.classList.add('product-name');
+	section_img.classList.add('product-img');
+	section_desc.classList.add('product-desc');
+	section_price.classList.add('product-price');
+	section_button.classList.add('add');
+
 	section_title.innerHTML = product.name;
-	section_img.innerHTML = product.img;
+	section_img.src = product.img;
+	section_img.alt = img_alt + product.name;
+	//section_img.setAttribute('src', product.img);               //ok also
+	//section_img.setAttribute('alt', img_alt + product.name);    //ok also
 	section_desc.innerHTML = product.desc;
 	section_price.innerHTML = "N" + product.price;
 	section_button.innerHTML = btn_label;
@@ -28,10 +39,49 @@ function createSection(product){
 }
 
 function updatePage(){
-	for (let key in products){
-		createSection(products[key]);
+	for (let key in Products){
+		createSection(Products[key]);
 	}
 }
 
+/*
+let cs = function createSection(product){
+	var section = document.createElement('section');
+	var section_title = document.createElement('h2');
+	var section_img = document.createElement('img');
+	var section_desc = document.createElement('p');
+	var section_price = document.createElement('p');
+	var section_button = document.createElement('button');
+
+	section.classList.add('product-item');
+	section_title.classList.add('product-name');
+	section_img.classList.add('product-img');
+	section_desc.classList.add('product-desc');
+	section_price.classList.add('product-price');
+	section_button.classList.add('add');
+
+	section_title.innerHTML = product.name;
+	section_img.src = product.img;
+	section_desc.innerHTML = product.desc;
+	section_price.innerHTML = "N" + product.price;
+	section_button.innerHTML = btn_label;
+
+	section.appendChild(section_title);
+	section.appendChild(section_img);
+	section.appendChild(section_desc);
+	section.appendChild(section_price);
+	section.appendChild(section_button);
+
+	main.appendChild(section);
+}
+
+let up = function updatePage(){
+	for (let key in Products){
+		cs(Products[key]);
+	}
+}
+*/
+
 //window.addEventListener('load', updatePage);
 window.addEventListener('DOMContentLoaded', updatePage);
+//window.addEventListener('DOMContentLoaded', up);
